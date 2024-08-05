b301bfdaeec581a1fa700e36bbdd9736a8ffe22f
James <rotichjames8@gmail.com>
2024-08-05 14:23:33 +0300

    responsive updates

--- src/Pages/EmptyCart.jsx
+++ src/Pages/EmptyCart.jsx
@@ -2,7 +2,7 @@ import { IoIosArrowRoundBack } from "react-icons/io";
 import { Link } from "react-router-dom";
 export const EmptyCart = () => {
   return (
-    <section className="px-10 max-sm:mt-[90px] max-sm:px-5 sm:mt-[145px] md:mr-[50%]">
+    <section className="min-h-screen px-10 max-sm:mt-[90px] max-sm:px-5 sm:mt-[145px] md:mr-[50%]">
       <Link to="/">
         <span className="mb-3 flex items-center hover:font-semibold">
           <IoIosArrowRoundBack />
