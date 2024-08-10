8ee5f4c9a2bd37514b8417ae935b4e880af9c491
James <rotichjames8@gmail.com>
2024-08-10 10:02:52 +0300

    updated empty cart page

--- src/Pages/EmptyCart.jsx
+++ src/Pages/EmptyCart.jsx
@@ -4,18 +4,16 @@ export const EmptyCart = () => {
   return (
     <section className="min-h-screen px-10 max-sm:mt-[90px] max-sm:px-5 sm:mt-[145px] md:mr-[50%]">
       <Link to="/">
-        <span className="mb-3 flex items-center hover:font-semibold">
+        <span className="mb-3 flex items-center hover:font-semibold max-sm:text-sm">
           <IoIosArrowRoundBack />
           <span> back to menu</span>
         </span>
       </Link>
 
-      <h1 className="upppercase max-sm:md text-xl font-semibold">
+      <h1 className="max-sm:md font-semibold uppercase md:text-xl">
         shopping bag
       </h1>
-      <div className="upppercase max-sm:text-sm">
-        Your shopping bag is empty
-      </div>
+      <div className="uppercase">Your shopping bag is empty</div>
     </section>
   );
 };
